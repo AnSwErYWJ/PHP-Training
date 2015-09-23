73ebfccf7e3b9bb85abb440cd4af0abfcb5ca0f8
WeiJie Yuan <yuanweijie1993@gmail.com>
2015-09-23 19:14:32 +0800

    20150920

---
+++ php/变量.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: answer
+ * Date: 2015/9/23
+ * Time: 19:07
+ */
+/*定义变量*/
+$var_name = "变量";
+$n =10;
+
+var_dump($var_name);//显示变量的数据类型和值
+echo $var_name;
+
+echo "<br />";
+
+var_dump($n);
+echo $n;
+
+?>
