a98c169174522c46e6e0a9936d3e79799bcfe6d9
WeiJie Yuan <yuanweijie1993@gmail.com>
2015-10-10 13:06:51 +0800

    20151010

---
+++ php/echo.php
@@ -0,0 +1,11 @@
+<?php
+ echo "Welcome to imooc!";
+ echo 12*3;//echo后有空格
+
+ /*下面三个语句相同*/
+ echo 'Hi,'.'imooc!';//连接符是用点（.）来表示的
+ echo "Hi,imooc！";
+ echo 'Hi,imooc！';
+
+echo "<br />";//换行
+?>
--- php/变量.php
+++ php/变量.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: answer
- * Date: 2015/9/23
- * Time: 19:07
- */
+
 /*定义变量*/
 $var_name = "变量";
 $n =10;
@@ -12,7 +7,7 @@ $n =10;
 var_dump($var_name);//显示变量的数据类型和值
 echo $var_name;
 
-echo "<br />";
+echo "<br />";//换行
 
 var_dump($n);
 echo $n;
